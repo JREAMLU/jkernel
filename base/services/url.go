ffe66e51970c228e2366e942ea22a32699e049dd
lujunjie <lujunjie.@plu.cn>
2016-06-22 09:57:18 +0800

    移除没有必要的代码

--- base/services/url.go
+++ base/services/url.go
@@ -1,11 +1,7 @@
 package services
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
-	"sort"
-	"strconv"
 
 	"github.com/JREAMLU/core/inout"
 	"github.com/JREAMLU/jkernel/base/services/atom"
@@ -65,8 +61,6 @@ func (r *Url) GoShorten(rawMetaHeader map[string][]string, rawDataBody []byte) (
 		return inout.EXPECTATION_FAILED, inout.OutputFail(checked.Message, "DATAPARAMSILLEGAL", checked.RequestID)
 	}
 
-	GenerateSign(rawDataBody, 1466490032, "kkkkk")
-
 	//进行shorten
 	var list = make(map[string]interface{})
 	var params_map = make(map[string]interface{})
@@ -91,106 +85,10 @@ func (r *Url) GoShorten(rawMetaHeader map[string][]string, rawDataBody []byte) (
 	data.List = list
 	data.Total = len(list)
 
-	// var batch int64
-	// batch = time.Now().Unix()
-	// fmt.Println("=======================", batch)
-
 	//持久化到mysql
 
 	// a := models.GetUrlOne()
 	// fmt.Println("AAAAAAAAAA", a["long_url"])
 
 	return inout.OK, inout.OutputSuccess(data, checked.MetaCheckResult["Request-Id"])
-}
-
-func GenerateSign(requestData []byte, requestTime int64, secretKey string) string {
-	//ksort
-	var rdata map[string]interface{}
-	json.Unmarshal([]byte(requestData), &rdata)
-
-	sorted_keys := make([]string, 0)
-	for k, _ := range rdata {
-		sorted_keys = append(sorted_keys, k)
-	}
-
-	sort.Strings(sorted_keys)
-
-	// var str string
-	//
-	// for _, k := range sorted_keys {
-	// 	fmt.Printf("k=%v, v=%v\n", k, rdata[k])
-	// 	str = str + k + rdata[k]
-	// }
-
-	// fmt.Println("==================", str)
-
-	str := Serialize(rdata)
-	fmt.Println("rrrrrr", str)
-
-	return ""
-}
-
-func Serialize(data interface{}) interface{} {
-	fmt.Println("----", data, "---", reflect.TypeOf(data).Kind())
-	var str string
-	switch reflect.TypeOf(data).Kind() {
-	case reflect.Slice:
-		s := reflect.ValueOf(data)
-		for i := 0; i < s.Len(); i++ {
-			serial := Serialize(s.Index(i).Interface())
-			if reflect.TypeOf(serial).Kind() == reflect.Float64 {
-				serial = strconv.Itoa(int(serial.(float64)))
-			}
-			str = str + strconv.Itoa(i) + serial.(string)
-		}
-		return str
-	case reflect.Map:
-		s := reflect.ValueOf(data)
-
-		//ksort
-		// sorted_keys := make([]string, 0)
-		// for k, _ := range rdata {
-		// 	sorted_keys = append(sorted_keys, k)
-		// }
-		// sort.Strings(sorted_keys)
-		// for _, k := range sorted_keys {
-		// 	fmt.Printf("k=%v, v=%v\n", k, rdata[k])
-		// 	str = str + k + rdata[k]
-		// }
-
-		keys := s.MapKeys()
-		//ksort
-		sorted_keys := make([]string, 0)
-		for _, key := range keys {
-			sorted_keys = append(sorted_keys, key.Interface().(string))
-		}
-		sort.Strings(sorted_keys)
-		for _, key := range sorted_keys {
-			serial := Serialize(s.MapIndex(reflect.ValueOf(key)).Interface())
-			if reflect.TypeOf(serial).Kind() == reflect.Float64 {
-				serial = strconv.Itoa(int(serial.(float64)))
-			}
-			str = str + key + serial.(string)
-		}
-		//     for _, key := range keys {
-		//         serial := Serialize(s.MapIndex(reflect.ValueOf(key.String())).Interface(), true)
-		//         if reflect.TypeOf(serial).Kind() == reflect.Float64 {
-		//             serial = strconv.Itoa(int(serial.(float64)))
-		//         }
-		//         str = str + key.String() + serial.(string)
-		//     }
-		// }
-		return str
-	}
-
-	fmt.Println("pppppp")
-	// fmt.Println(data)
-	// switch data.(type) {
-	// case []interface{}:
-	// case map[string]interface{}:
-	// 	for k, v := range data {
-	//
-	// 	}
-	// }
-	return data
 }
